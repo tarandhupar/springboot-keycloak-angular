74057e63e5b2b4955d1c514e15ca151b30bcfbcb
Tarandeep Dhupar <taran.github@gmail.com>
2025-02-12 11:56:21 -0500

    Initial check in

--- spring-app/src/main/java/com/example/springapp/controller/WebController.java
+++ spring-app/src/main/java/com/example/springapp/controller/WebController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class WebController {
 
